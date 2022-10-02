cc05039e19c4e8c55a9bd8ecd23f7b16497410ab
Cleo Gao <celeo127@outlook.com>
2022-10-02 09:26:20 +0800

    finish styling checkout success page

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,4 +1,5 @@
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon, ShoppingCartIcon } from "@heroicons/react/outline";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,12 +7,25 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import { Button } from "../components/Button";
+import { fetchLineItems } from '../utils/fetchLineItems';
 
-const Success = () => {
+interface Props {
+    products: StripeProduct;
+}
+
+const Success = ({ products} : Props) => {
+    console.log(products);
+  
+
     const router = useRouter();
     const { session_id } = router.query;
     const [mounted, setMounted] = useState(false);
     const [showOrderSummary, setShowOrderSummary] = useState(false);
+    
+    const subtotal = products.reduce(
+        (acc, product) => acc + product.price.unit_amount / 100,
+        0
+      );
 
 
     useEffect(() => {
@@ -94,7 +108,8 @@ const Success = () => {
                         </p>
                     </div>
 
-                    <div>
+                    <div className="mx-4 flex flex-col items-center justify-between
+                    text-sm lg:ml-14 lg:flex-row">
                         <p className="hidden lg:inline">Need help? Contact us</p>
                         {mounted && (
                             <Button
@@ -108,7 +123,8 @@ const Success = () => {
                     </section>
 
                         {mounted && (
-                            <section>
+                            <section className="overflow-y-scroll border-y border-l border-gray-300 bg-[#FAFAFA] lg:order-2 lg:col-span-4 lg:h-screen
+                            lg:border-y-0">
                                 <div className={`w-full ${showOrderSummaryCondition && "border-b"} border-gray-300 text-sm lg:hidden`}>
                                     <div className="mx-auto flex max-w-xl items-center justify-between px-4 py-6">
                                         <button onClick={handleShowOrderSummary} 
@@ -123,12 +139,61 @@ const Success = () => {
                                             )}
                                         </button>
 
-                                        <p className="text-wl font-medium text-black"></p>
+                                        <p className="text-wl font-medium text-black">
+                                            ${subtotal + 20}
+                                        </p>
                                     </div>
                                 </div>
                                 {showOrderSummaryCondition && (
-                                    <div>
-                                        <div></div>
+                                    <div className="mx-auto max-w-xl divide-y border-gray-300 px-4 py-4 lg:mx-0 lg:max-w-lg lg:px-10 lg:py-16">
+                                        <div className="space-y-4 pb-4">
+                                            {products.map((product) => (
+                                                <div key={product.id}
+                                                className="flex items-center space-x-4 text-sm font-medium"
+                                                >
+                                                    <div className="relative flex h-16 w-16 items-center justify-center 
+                                                    border border-gray-300 bg-[#F1F1F1] text-xs text-white">
+                                                        <div className="relative h-7 w-7 animate-bounce rounded-md">
+                                                        <Image src="https://rb.gy/vsvv2o"
+                                                               layout='fill'
+                                                                objectFit="contain" />
+                                                        </div>
+                                                            <div className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-[gray] text-xs">
+                                                                {product.quantity}
+                                                            </div>
+                                                        </div>
+                                                        <p className="flex-1">{product.description}</p>
+                                                        <p>
+                                                            ${product.price.unit_amount/100}
+                                                            {/* ${product.currency} */}
+                                                        </p>
+                                                </div>
+                                            ))}
+                                        </div>
+                                        <div className="space-y-1 py-4">
+                                            <div className="flex justify-between text-sm">
+                                                <p className="text-[gray]">Subtotal</p>
+                                                <p className="font-medium">
+                                                {subtotal}
+                                                </p>   
+                                            </div>
+                                            <div className="flex justify-between text-sm">
+                                                <p className="text-[gray]">Discount</p>
+                                                <p className="text-[gray]"></p>
+                                            </div>
+                                            <div className="flex jusfity-between text-sm">
+                                                <p className="text-[gray]">Shipping</p>
+                                                <p className="font-medium">${20}</p>
+                                            </div>
+                                        </div>
+                                        <div className="flex justify-between pt-4">
+                                            <p>Total</p>
+                                            <p className="flex items-center gap-x-2 text-xs text-[gray]">USD
+                                            <span className="text-xl font-medium text-black">
+                                                {subtotal + 20}
+                                            </span>
+                                            </p>
+                                        </div>
                                     </div>
                                 )}
                             </section>
@@ -139,3 +204,15 @@ const Success = () => {
 }
 
 export default Success;
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({query}) => {
+    const sessionId = query.session_id as string;
+    const products = await fetchLineItems(sessionId);
+
+    return{
+        props: {
+            products,
+        },
+    };
+
+}
